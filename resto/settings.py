6d63afa7541d2bc536bc552d73b845f9507c59cb
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-29 23:16:19 +0530

    origin added in settings

--- resto/settings.py
+++ resto/settings.py
@@ -96,6 +96,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://at-table-frontend.vercel.app/"
 ]
 
 # REST Framework Configuration
